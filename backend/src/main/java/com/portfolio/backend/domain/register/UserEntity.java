ae2c9a48f25907456c708d8b525010999084f040
Lucas <lupelikan@int.pl>
2025-05-12 19:32:25 +0300

    created posts and comments tables with corresponding entities

--- backend/src/main/java/com/portfolio/backend/domain/register/UserEntity.java
+++ backend/src/main/java/com/portfolio/backend/domain/register/UserEntity.java
@@ -1,5 +1,6 @@
 package com.portfolio.backend.domain.register;
 
+import com.portfolio.backend.domain.blog.CommentEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -44,6 +45,15 @@ public class UserEntity implements UserDetails {
             inverseJoinColumns = {@JoinColumn(name = "role_id")}
     )
     private Set<RoleEntity> roleEntities = new HashSet<>();
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "user")
+    private Set<CommentEntity> comments = new HashSet<>();
+
+    public UserEntity(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
